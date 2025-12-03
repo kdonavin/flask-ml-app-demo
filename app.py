3f8d6bba4afc8d0c9153fe4469ce81eba281d762
Kirkwood Donavin <agentkirkwood@gmail.com>
2025-12-02 20:29:29 -0700

    incorporating galvanize soln

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
-from src.build_model import TextClassifier
+from soln_build_model import TextClassifier
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template('index.html')
 
-@app.route('/submit')
-def submission_page():
-	return render_template('submit.html')
+with open('static/model.pkl') as f:
+    model = pickle.load(f)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """Render a simple splash page."""
+    return render_template('form/index.html')
+
+@app.route('/submit', methods=['GET'])
+def submit():
+    """Render a page containing a textarea input where the user can paste an
+    article to be classified.  """
+    return render_template('form/submit.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-	with open('static/model.pkl', 'rb') as f:
-		model = pickle.load(f)  
-	text = str(request.form['article_body'])
-	pred = model.predict([text])[0]
-	return render_template('predict.html', article=text, predicted=pred)
+    """Recieve the article to be classified from an input form and use the
+    model to classify.
+    """
+    data = str(request.form['article_body'])
+    pred = str(model.predict([data])[0])
+    return render_template('form/predict.html', article=data, predicted=pred)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', debug=True)
