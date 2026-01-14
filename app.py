585b9cf1c385d5a8474e0fa093523fd95de2cfe3
Kirkwood Donavin <agentkirkwood@gmail.com>
2026-01-14 02:12:50 -0700

    loading model lazily

--- app.py
+++ app.py
@@ -12,8 +12,14 @@ def bootstrap_static(filename):
     return send_from_directory(bootstrap_dir, filename)
 
 
-with open('static/model.pkl', 'rb') as f:
-    model = pickle.load(f)
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        with open('static/model.pkl', 'rb') as f:
+            model = pickle.load(f)
+    return model
 
 @app.route('/', methods=['GET'])
 def index():
@@ -59,6 +65,7 @@ def predict():
             }), 400
         
         # Make prediction with probability
+        model = get_model()
         prediction = str(model.predict([article_text])[0])
         probabilities = model.predict_proba([article_text])[0]
         max_probability = float(max(probabilities))
@@ -81,6 +88,7 @@ def predict_traditional():
     """Traditional form submission: Receive article from form and return HTML page.
     """
     data = str(request.form['article_body'])
+    model = get_model()
     pred = str(model.predict([data])[0])
     probabilities = model.predict_proba([data])[0]
     max_prob = float(max(probabilities))
